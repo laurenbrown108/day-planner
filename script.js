d2f588a6eb1c938e46617d2fa4daa29303ba0236
Lauren Brown <laurenbrown108@gmail.com>
2020-09-11 21:34:13 -0400

    Local storage saved to page & finishes touches.

--- script.js
+++ script.js
@@ -1,73 +1,52 @@
-var event
-var time
+var event;
+var time;
 
 $(document).ready(function() {
+
+    //Display saved entries upon page load
+    displayEntries ();
     
     //display current date in jumbotron
     $("#currentDay").text("Today's Date: " + moment().format("MMM Do YY"))
 
-
     //background colors & current time
 
     var currentHour = moment().format("H");
-    console.log(currentHour);
-        for(var i = 0; i <= 17; i++) {
-            var possTime = i
-            //console.log(possTime)
-            if (parseInt(currentHour) === possTime) {
-                $("#" + possTime).addClass("present")
-            }
-            else if (parseInt(currentHour) < possTime) {
-                $("#" + possTime).addClass("future")
-            }
-            else {
-                $("#" + possTime).addClass("past")
-            }
+    //console.log(currentHour);
+    for(var i = 0; i <= 17; i++) {
+        var possTime = i
+        //console.log(possTime)
+        if (parseInt(currentHour) === possTime) {
+            $("#" + possTime).addClass("present")
         }
-        /////^^^ might have to add parseIn to else if/else
+        else if (parseInt(currentHour) < possTime) {
+            $("#" + possTime).addClass("future")
+        }
+        else {
+            $("#" + possTime).addClass("past")
+        }
+    }
 
-    //on Click save button, save user input from corresponding....class? or parent/child?
+    //Click functions for buttons to save input and time.
     $(".saveBtn").on("click", function() {
         time = $(this).parent().attr("id")
         event = $(this).parent().children("input").val()
-        //console.log(event);
-        //console.log(time);
-        
-        
+        localStorage.setItem(time, event);
+        localStorage.getItem(time);
+        console.log(localStorage.getItem(time))
     })
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //renders local data
+    function displayEntries() {
+        
+            $("#9").val(localStorage.getItem("9am"))
+            $("#10").val(localStorage.getItem("10am"))
+            $("#11").val(localStorage.getItem("11am"))
+            $("#12").val(localStorage.getItem("12pm"))
+            $("#13").val(localStorage.getItem("1pm"))
+            $("#14").val(localStorage.getItem("2pm"))
+            $("#15").val(localStorage.getItem("3pm"))
+            $("#16").val(localStorage.getItem("4pm"))
+            $("#17").val(localStorage.getItem("5pm"))
+        
+    }
 })
